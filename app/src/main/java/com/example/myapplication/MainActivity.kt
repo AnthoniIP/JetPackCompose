724737e24042f8dfa23508d466b27abd27035c41
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-01-16 16:11:53 -0300

    Feito o primeiro elemento de UI, a função NewsStory()

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,62 +1,77 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.HorizontalGradient
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.ui.theme.typography
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            NewsStory()
 
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Olá $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
 
 @Composable
-fun RowsAndColumns() {
-    Column(
-            modifier = Modifier
-                    .padding(16.dp)
+fun NewsStory() {
+    val image = imageResource(id = R.drawable.header)
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(
+                modifier = Modifier.padding(16.dp)
+        ) {
+
+            val imageModifier = Modifier
+                    .preferredHeight(180.dp)
                     .fillMaxWidth()
-    ) {
-        Text("Coluna 1")
-        Text("Coluna 2")
-        Text("Coluna 3")
-    }
-    Row(
+                    .clip(shape = RoundedCornerShape(4.dp))
 
-    ) {
-        Text("Linha 1")
-        Text("Linha 2")
-        Text("Linha 3")
-    }
+            Image(image,
+                    modifier = imageModifier,
+                    contentScale = ContentScale.Crop)
 
+            Spacer(modifier = Modifier.preferredHeight(16.dp))
+
+            Text("A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis)
+            Text("Devenport, California",
+                    style = typography.body2)
+            Text("December 2018",
+                    style = typography.body2)
+
+        }
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun DefaultPreview() {
+    NewsStory()
 }
